7d7f13b2cf0f2220beec057a17e86f06191dbb55
kamitoshi <address>
2020-03-17 08:41:04 +0000

    [finish] 4-8 エラーメッセージ実装

--- README.md
+++ README.md
@@ -3,6 +3,11 @@
 ## [start] 4-8 bookers
 アプリケーションの立ち上げと、コントローラーの作成
 
+## [update] 1-8 エラー機能実装前
+エラーメッセージの実装を行う前の段階 commit commentは4-8の間違えです。
+
+## [finish] 4-8 エラーメッセージ実装
+
 This README would normally document whatever steps are necessary to get the
 application up and running.
 
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,13 +4,18 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book)
-    flash[:notice] = "Book was successfully created."
+    @book = Book.new(book_params)
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def show
@@ -22,10 +27,13 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book)
-    flash[:notice] = "Book was successfully updated."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+      flash[:notice] = "Book was successfully updated."
+    else
+      render :edit
+    end
   end
 
   def destroy
